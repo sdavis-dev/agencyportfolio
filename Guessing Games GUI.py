456634b7047fa702f717c4d7ca07ff50232e3d37
SeDerrick <sederrickdavisbup@gmail.com>
2024-10-19 02:33:19 -0500

    v1.0 GUI

---
+++ Guessing Games GUI.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title('Guessing Games')
+root.geometry("325x200")
+
+guessStatement = ttk.Label(root, text="Guess a number between 1 and 100 (or click 'quit' to give up)")
+guessStatement.grid(row = 0, column = 0, columnspan= 5)
+
+playerGuess = tk.IntVar()
+playerGuessEntry = ttk.Entry(root, width = 10, textvariable = playerGuess)
+playerGuessEntry.grid(row = 1, column = 0, columnspan = 5)
+
+
+
+root.mainloop()
