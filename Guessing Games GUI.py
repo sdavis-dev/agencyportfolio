e74d7d95d82dc1dd8d09fa89832319dfab6ed11b
SeDerrick <sederrickdavisbup@gmail.com>
2024-10-19 18:30:08 -0500

    v1.1 GUI

--- Guessing Games GUI.py
+++ Guessing Games GUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import random
 
 root = tk.Tk()
 root.title('Guessing Games')
@@ -12,10 +13,24 @@ playerGuess = tk.IntVar()
 playerGuessEntry = ttk.Entry(root, width = 10, textvariable = playerGuess)
 playerGuessEntry.grid(row = 1, column = 0, columnspan = 5)
 
-#This is for the button to submit the player's guess
-submitGuessBtn = tk.Button(root, text="Submit Guess", command = playerGuess)
-submitGuessBtn.grid(row=2, column=0, columnspan=5)
+# Random function to create random number and guess functionality
+randNumGen = random.randint(1, 100)
+gameAnswer = randNumGen
+def random():
+    if playerGuess.get() == gameAnswer:
+        resultLabel.config(text="Congrat you guessed the right Number!!!") # Configure label to print text
+    elif playerGuess.get() > gameAnswer:
+        resultLabel.config(text="Wrong Answer! Guess a lower number Try again.") # Configure label to print text
+    elif playerGuess.get() < gameAnswer:
+        resultLabel.config(text="Wrong Answer! Guess a higher number Try again.") # Configure label to print text
 
+# Label to display if player guess is right or wrong
+resultLabel = ttk.Label(root, text="")
+resultLabel.grid(row = 3, column = 0, columnspan = 5)
+
+#This is for the button to submit the player's guess
+submitGuessBtn = tk.Button(root, text="Submit Guess", command = random)
+submitGuessBtn.grid(row=2, column=0, columnspan=5)
 
 
 root.mainloop()
